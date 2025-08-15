9e36f3a43a80cf64b0494582fc2ee2bd8b25096c
rush4sg80 <rush4sg80@gmail.com>
2025-08-16 02:02:33 +0530

    Create streamlit_app_pending.py
    Pending Orders

---
+++ streamlit_app_pending.py
@@ -0,0 +1,65 @@
+# Import python packages
+import pandas as pd
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+# Write directly to the app
+st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw: ")
+st.write(
+  """Orders That Need to be Filled
+  """
+)
+
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+
+if my_dataframe:
+    editable_df=st.data_editor(my_dataframe)
+    Submiited=st.button('Submit')
+
+    if Submiited:
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+
+        try:
+            og_dataset.merge(edited_dataset
+                     , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})])
+
+            st.success('Someone clicked the button', icon = '👍')
+        except:
+            st.write( """Something Went Wrong""")
+
+else:
+    st.success('No pending order write now', icon = '👍')
+
+                
+    
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+     
+    
